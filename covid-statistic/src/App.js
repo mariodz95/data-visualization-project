29f6a7e00346e6de296133f91c504086cb7a589c
mariodz95 <mariodz95@gmail.com>
2020-05-25 16:51:15 +0200

    Line graph

--- covid-statistic/src/App.js
+++ covid-statistic/src/App.js
@@ -4,6 +4,13 @@ import * as d3 from "d3";
 import { drawMap } from "./drawMap";
 import { lineChart } from "./LineChart";
 import LineChart from "./test";
+import Select from "react-select";
+
+const options = [
+  { value: "cases", label: "Cases" },
+  { value: "deaths", label: "Deaths" },
+  { value: "cured", label: "Cured" },
+];
 
 class App extends React.Component {
   state = {
@@ -15,6 +22,7 @@ class App extends React.Component {
     curedInCroatiaLineGraphData: [],
     deathsInCroatiaLineGraphData: [],
     flag: true,
+    selectedOption: null,
   };
 
   componentDidMount() {
@@ -42,7 +50,7 @@ class App extends React.Component {
           this.state.lineGraphData.push(covidCase);
         }
         // lineChart(this.state.lineGraphData);
-        this.lineChart2(true);
+        this.lineChart2("cases");
         drawMap();
       });
 
@@ -56,8 +64,7 @@ class App extends React.Component {
   }
 
   lineChart2 = (data) => {
-    console.log("data u funkciji", data);
-    d3.select(".test").remove();
+    console.log("data u funkciji", data.value);
 
     // set the dimensions and margins of the graph
     var margin = { top: 20, right: 20, bottom: 30, left: 50 },
@@ -75,10 +82,12 @@ class App extends React.Component {
         return x(new Date(d.date));
       })
       .y(function (d) {
-        if (data === true) {
+        if (data.value === "cases") {
           return y(d.cases);
-        } else {
+        } else if (data.value === "deaths") {
           return y(d.deaths);
+        } else if (data.value === "cured") {
+          return y(d.cured);
         }
       });
 
@@ -103,10 +112,12 @@ class App extends React.Component {
     y.domain([
       0,
       d3.max(this.state.lineGraphData, function (d) {
-        if (data === true) {
+        if (data.value === "cases") {
           return d.cases;
-        } else {
+        } else if (data.value === "deaths") {
           return d.deaths;
+        } else if (data.value === "cured") {
+          return d.cured;
         }
       }),
     ]);
@@ -120,60 +131,132 @@ class App extends React.Component {
     // Add the X Axis
     svg
       .append("g")
+      .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
     // Add the Y Axis
-    svg.append("g").call(d3.axisLeft(y));
+    svg.append("g").attr("class", "y axis").call(d3.axisLeft(y));
   };
 
-  drawBarChart() {
-    var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    var width = 500;
-    var height = 500;
-    var svg = d3
-      .select("body")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height);
-    var barchart = svg
-      .selectAll("rect")
-      .data(this.state.casesInCroatia)
-      .enter()
-      .append("rect")
-      .attr("x", function (d, i) {
-        return 50 * i;
-      })
-      .attr("y", function (d) {
-        return height - d * 50;
-      })
-      .attr("width", 40)
-      .attr("height", function (d) {
-        return d * 50;
-      })
-      .attr("fill", "blue");
-    var width = 500;
-    var height = 500;
+  // ** Update data section (Called from the onclick)
+  updateData = (data) => {
+    console.log("update", data);
+    // set the dimensions and margins of the graph
+    var margin = { top: 20, right: 20, bottom: 30, left: 50 },
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+    // set the ranges
+    var x = d3.scaleTime().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
 
-    var svg = d3
-      .select("body")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height);
-  }
+    // define the line
+    var valueline = d3
+      .line()
+      .x(function (d) {
+        return x(new Date(d.date));
+      })
+      .y(function (d) {
+        if (data.value === "cases") {
+          return y(d.cases);
+        } else if (data.value === "deaths") {
+          return y(d.deaths);
+        } else if (data.value === "cured") {
+          return y(d.cured);
+        }
+      });
+    // Scale the range of the data again
+    x.domain(
+      d3.extent(this.state.lineGraphData, function (d) {
+        return new Date(d.date);
+      })
+    );
+    y.domain([
+      0,
+      d3.max(this.state.lineGraphData, function (d) {
+        if (data.value === "cases") {
+          return d.cases;
+        } else if (data.value === "deaths") {
+          return d.deaths;
+        } else if (data.value === "cured") {
+          return d.cured;
+        }
+      }),
+    ]);
+
+    // Select the section we want to apply our changes to
+    var svg = d3.select("body").transition();
+
+    // Make the changes
+    svg
+      .select(".line") // change the line
+      .duration(750)
+      .attr("d", valueline(this.state.lineGraphData));
+    svg
+      .select(".x.axis") // change the x axis
+      .duration(750)
+      .call(d3.axisBottom(x));
+    svg
+      .select(".y.axis") // change the y axis
+      .duration(750)
+      .call(d3.axisLeft(y));
+  };
+  // drawBarChart() {
+  //   var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  //   var width = 500;
+  //   var height = 500;
+  //   var svg = d3
+  //     .select("body")
+  //     .append("svg")
+  //     .attr("width", width)
+  //     .attr("height", height);
+  //   var barchart = svg
+  //     .selectAll("rect")
+  //     .data(this.state.casesInCroatia)
+  //     .enter()
+  //     .append("rect")
+  //     .attr("x", function (d, i) {
+  //       return 50 * i;
+  //     })
+  //     .attr("y", function (d) {
+  //       return height - d * 50;
+  //     })
+  //     .attr("width", 40)
+  //     .attr("height", function (d) {
+  //       return d * 50;
+  //     })
+  //     .attr("fill", "blue");
+  //   var width = 500;
+  //   var height = 500;
+
+  //   var svg = d3
+  //     .select("body")
+  //     .append("svg")
+  //     .attr("width", width)
+  //     .attr("height", height);
+  // }
 
   change = () => {
     this.setState({ flag: false });
     this.lineChart2(false);
   };
+
+  handleChange = (selectedOption) => {
+    this.setState({ selectedOption });
+    console.log(`Option selected:`, selectedOption);
+    // this.lineChart2(selectedOption);
+    this.updateData(selectedOption);
+  };
   render() {
-    console.log("Test", this.state.covidStatus);
-    // if (this.state.lineGraphData.length > 0) {
-    //   this.lineChart2(false);
-    // }
+    const { selectedOption } = this.state;
 
     return (
       <React.Fragment>
+        <Select
+          value={selectedOption}
+          onChange={this.handleChange}
+          options={options}
+        />
         <button onClick={this.change}>Activate Lasers</button>
         {/* <LineChart props={this.state.lineGraphData}></LineChart> */}
         <svg className="test">
