746876ad7d1b7fb192a3200a85e504110f286136
mariodz95 <mariodz95@gmail.com>
2020-06-02 18:49:59 +0200

    Add barchart work in progress

--- covid-statistic/src/App.js
+++ covid-statistic/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { drawMap } from "./drawMap";
 import Select from "react-select";
 import LineChartData from "./helpers/LineChartData";
+import BarChartData from "./helpers/BarChartData";
 import PieData from "./helpers/PieData";
 import GenderData from "./helpers/GenderData";
 import { lineChart } from "./lineChart";
@@ -10,6 +11,7 @@ import { updateData } from "./updateData";
 import { options } from "./helpers/dropDownConstants";
 import covid from "./covid.png";
 import { drawPieChart } from "./pieChart";
+import * as d3 from "d3";
 
 class App extends React.Component {
   state = {
@@ -23,6 +25,7 @@ class App extends React.Component {
     infectedByGender: [],
     gender: false,
     update: false,
+    barChartData: [],
     states: [],
     statesInfected: [],
     statesDeaths: [],
@@ -42,7 +45,6 @@ class App extends React.Component {
         return data.json();
       })
       .then((data) => {
-        console.log("zadnji podaci ", data[0].PodaciDetaljno);
         for (let i = 0; i < data[0].PodaciDetaljno.length; i++) {
           this.state.dataByState.push(data[0].PodaciDetaljno[i]);
           this.state.states.push(data[0].PodaciDetaljno[i].Zupanija);
@@ -50,10 +52,16 @@ class App extends React.Component {
             data[0].PodaciDetaljno[i].broj_zarazenih
           );
           this.state.statesDeaths.push(data[0].PodaciDetaljno[i].broj_umrlih);
+          var test = new BarChartData(
+            data[0].PodaciDetaljno[i].broj_zarazenih,
+            data[0].PodaciDetaljno[i].Zupanija
+          );
+          this.state.barChartData.push(test);
         }
-        console.log("zadnji podaci ", this.state.states);
+        console.log("zadnji podaci ", this.state.barChartData);
 
         drawMap(this.state.dataByState);
+        this.drawBarChart();
       });
 
     fetch("https://www.koronavirus.hr/json/?action=podaci")
@@ -132,6 +140,130 @@ class App extends React.Component {
       .then((data) => {});
   }
 
+  drawBarChart = () => {
+    const width = 800;
+    const height = 450;
+    const svg = d3
+      .select("body")
+      .select(".barChart")
+      .attr("id", "chart")
+      .attr("width", width)
+      .attr("height", height);
+    const margin = {
+      top: 60,
+      bottom: 100,
+      left: 80,
+      right: 40,
+    };
+
+    const chart = svg
+      .append("g")
+      .classed("display", true)
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    const chartWidth = width - margin.left - margin.right;
+    const chartHeight = height - margin.top - margin.bottom;
+    this.plot(chart, chartWidth, chartHeight);
+  };
+
+  plot = (chart, chartWidth, chartHeight) => {
+    const width = 800;
+    const height = 300;
+    const xScale = d3
+      .scaleBand()
+      .domain(this.state.barChartData.map((d) => d.state))
+      .range([0, width]);
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(this.state.barChartData, (d) => d.value)])
+      .range([height, 0]);
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+    chart
+      .selectAll(".bar")
+      .data(this.state.barChartData)
+      .enter()
+      .append("rect")
+      .classed("bar", true)
+      .attr("x", (d) => xScale(d.state))
+      .attr("y", (d) => yScale(d.value))
+      .attr("height", (d) => height - yScale(d.value))
+      .attr("width", (d) => xScale.bandwidth())
+      .style("fill", (d, i) => colorScale(i));
+
+    chart
+      .selectAll(".bar-label")
+      .data(this.state.barChartData)
+      .enter()
+      .append("text")
+      .classed("bar-label", true)
+      .attr("x", (d) => xScale(d.state) + xScale.bandwidth() / 2)
+      .attr("dx", 0)
+      .attr("y", (d) => yScale(d.value))
+      .attr("dy", -6)
+      .text((d) => d.value);
+
+    const xAxis = d3.axisBottom().ticks(25).scale(xScale);
+
+    // Add the X Axis
+    chart
+      .append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+      .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", ".15em")
+      .attr("transform", function (d) {
+        return "rotate(-65)";
+      });
+
+    // chart
+    //   .append("g")
+    //   .classed("x axis", true)
+    //   .attr("transform", `translate(0,${height})`)
+    //   .call(xAxis);
+
+    const yAxis = d3.axisLeft().ticks(5).scale(yScale);
+
+    chart
+      .append("g")
+      .classed("y axis", true)
+      .attr("transform", "translate(0,0)")
+      .call(yAxis);
+
+    // chart
+    //   .select(".x.axis")
+    //   .append("text")
+    //   .attr("x", width / 2)
+    //   .attr("y", 60)
+    //   .attr("transform", `translate(0, ${height})`)
+    //   .attr("fill", "#000")
+    //   .style("font-size", "1px")
+    //   .style("text-anchor", "middle")
+    //   .text("Županija");
+
+    chart
+      .select(".y.axis")
+      .append("text")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("transform", `translate(-50, ${height / 2}) rotate(-90)`)
+      .attr("fill", "#000")
+      .style("font-size", "20px")
+      .style("text-anchor", "middle")
+      .text("Government Expenditure in Billion Dollars");
+
+    const yGridlines = d3
+      .axisLeft()
+      .scale(yScale)
+      .ticks(5)
+      .tickSize(-width, 0, 0)
+      .tickFormat("");
+
+    chart.append("g").call(yGridlines).classed("gridline", true);
+  };
   handleChange = (selectedOption) => {
     this.setState({ selectedOption });
     updateData(selectedOption, this.state.lineGraphData);
@@ -207,6 +339,8 @@ class App extends React.Component {
             </div>
           </div>
         </div>
+        <br />
+        <svg className="barChart"></svg>
         <br />
         <br />
       </React.Fragment>
---
+++ covid-statistic/src/helpers/BarChartData.js
@@ -0,0 +1,6 @@
+export default class BarChartData {
+  constructor(value, state) {
+    this.value = value;
+    this.state = state;
+  }
+}
