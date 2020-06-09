f481b3f145ba8facbd4618e7974256812a2f815a
mariodz95 <mariodz95@gmail.com>
2020-06-09 15:34:27 +0200

    Updates

---
+++ precache-manifest.dca7895ded00cf9e543b1017aba18297.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c313c93731c8d1fd6cbd0a08da640e7e",
+    "url": "/data-visualization-project/index.html"
+  },
+  {
+    "revision": "4de4573f745a7cf464d6",
+    "url": "/data-visualization-project/static/css/main.71626bb3.chunk.css"
+  },
+  {
+    "revision": "3c36ad8852eba10f48b7",
+    "url": "/data-visualization-project/static/js/2.f9f94cc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/data-visualization-project/static/js/2.f9f94cc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de4573f745a7cf464d6",
+    "url": "/data-visualization-project/static/js/main.13a57223.chunk.js"
+  },
+  {
+    "revision": "bcf64b24b61bd6cc5c74",
+    "url": "/data-visualization-project/static/js/runtime-main.5d9ee129.js"
+  },
+  {
+    "revision": "354d7b402cc2b90a31c60d0e26321858",
+    "url": "/data-visualization-project/static/media/covid.354d7b40.png"
+  }
+]);
